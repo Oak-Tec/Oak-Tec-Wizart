1d30ec5385c8aa224b4d728653f1b2347247ec5f
Oak-Tec <kaserbacherf@proton.me>
2025-01-10 18:17:43 +0000

    Create uservar.py

---
+++ uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'http://funstersplace.net/text/builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'http://funstersplace.net/text/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
