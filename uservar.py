9d6bd78549b99bb3d234230a8ecec88c0f9437c9
Oak-Tec <kaserbacherf@proton.me>
2025-01-11 23:25:24 +0100

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -2,9 +2,3 @@ import xbmcaddon
 
 '''#####-----Build File-----#####'''
 buildfile = 'https://file.io/SMn08apvNqJx'
-
-'''#####-----Notifications File-----#####'''
-notify_url  = 'http://funstersplace.net/text/notify.txt'
-
-'''#####-----Excludes-----#####'''
-excludes  = ['plugin.video.whatever']
